fa9f33e237d5812dce5dc3ff410c3720be37407f
Justin Kelly <Justin.Kelly@arcoro.com>
2025-11-07 15:47:54 -0600

    fix

--- apps/test-app/next.config.ts
+++ apps/test-app/next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_R2_HOST!,
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: process.env.NEXT_PUBLIC_R2_HOST!,
+  //       port: "",
+  //       pathname: "/**",
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
